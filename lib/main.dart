9b70bc71c6368912f70ca963851e1b6b9d271161
Bilal Aamer <bilalahmedaamer@gmail.com>
2023-03-17 23:58:25 +0530

    Primary Color to blue

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.amber,
+        primarySwatch: Colors.blue,
       ),
       home: const HomePage(),
     );
