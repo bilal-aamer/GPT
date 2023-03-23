1e464619aafb8cfbc4cd4dc71498f18c3387a42b
Bilal Aamer <bilalahmedaamer@gmail.com>
2023-03-23 23:56:47 +0530

    Title Change

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'GPT Home',
       debugShowCheckedModeBanner: true,
       theme: ThemeData(
         primarySwatch: Colors.blue,
