cfe89751836b524c9f32f31b5fae3e6b0274b448
Bilal Aamer <bilalahmedaamer@gmail.com>
2023-03-30 15:39:20 +0530

    test

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'GPT Home',
-      debugShowCheckedModeBanner: ,
+      debugShowCheckedModeBanner: true,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
